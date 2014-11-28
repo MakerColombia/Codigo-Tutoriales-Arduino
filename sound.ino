fa10f497e13efda85e363ff1918de27d3312c41a
Julio Aguirre <jcallano@gmail.com>
2014-11-28 18:36:38 -0500

    Create sound.ino

---
+++ sound.ino
@@ -0,0 +1,33 @@
+/*
+This is the code to make a LED blink with sound.
+You have to set the threshold so it' sensible enough to make the led blink.
+ 
+You connect an LED to PIN13 and the Sound Sensor to Analog Pin 0
+ */
+ 
+int led = 13;
+int threshold = 22; //Change This
+int volume;
+ 
+void setup() {                
+  Serial.begin(9600); // For debugging
+  pinMode(led, OUTPUT);     
+}
+ 
+void loop() {
+  
+  volume = analogRead(A0); // Reads the value from the Analog PIN A0
+  /*
+    //Debug mode
+    Serial.println(volume);
+    delay(100);
+  */
+  
+  if(volume>=threshold){
+    digitalWrite(led, HIGH); //Turn ON Led
+  }  
+  else{
+    digitalWrite(led, LOW); // Turn OFF Led
+  }
+ 
+}
